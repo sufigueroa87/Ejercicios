3e8b611a611dc5d38d41c041b036895db27dad62
sufigueroa87 <sufigueroa@proton.me>
2023-06-05 23:46:53 +0200

    Modifico el método generaDNI.

--- 59. Declaración de clases · Ejercicio 0007/Persona.java
+++ 59. Declaración de clases · Ejercicio 0007/Persona.java
@@ -133,79 +133,8 @@ public class Persona {
     public String generaDNI(){
         int dniNumeros = numeroAleatorio8Cifras();
         int letraDNInumero = dniNumeros % 23;
-
-        char letraDNI = ' ';
-        switch(letraDNInumero){
-            case(0):
-                letraDNI = 'T';
-                break;
-            case(1):
-                letraDNI = 'R';
-                break;
-            case(2):
-                letraDNI = 'W';
-                break;
-            case(3):
-                letraDNI = 'A';
-                break;
-            case(4):
-                letraDNI = 'G';
-                break;
-            case(5):
-                letraDNI = 'M';
-                break;
-            case(6):
-                letraDNI = 'Y';
-                break;
-            case(7):
-                letraDNI = 'F';
-                break;
-            case(8):
-                letraDNI = 'P';
-                break;
-            case(9):
-                letraDNI = 'D';
-                break;
-            case(10):
-                letraDNI = 'X';
-                break;
-            case(11):
-                letraDNI = 'B';
-                break;
-            case(12):
-                letraDNI = 'N';
-                break;
-            case(13):
-                letraDNI = 'J';
-                break;
-            case(14):
-                letraDNI = 'Z';
-                break;
-            case(15):
-                letraDNI = 'S';
-                break;
-            case(16):
-                letraDNI = 'Q';
-                break;
-            case(17):
-                letraDNI = 'V';
-                break;
-            case(18):
-                letraDNI = 'H';
-                break;
-            case(19):
-                letraDNI = 'L';
-                break;
-            case(20):
-                letraDNI = 'C';
-                break;
-            case(21):
-                letraDNI = 'K';
-                break;
-            case(22):
-                letraDNI = 'E';
-                break;
-        }
+        char[] letrasDNI = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        char letraDNI = letrasDNI[letraDNInumero];
         String dniGenerado = (dniNumeros + "") + (letraDNI + "");
         return dniGenerado;
     }
